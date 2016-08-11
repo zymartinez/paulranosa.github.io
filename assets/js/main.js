b6b905bef534711e044acc200f6d2c556c693c0a
paulranosa <pjranosa@gmail.com>
2016-08-12 04:34:51 +0800

    Thursday

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,9 @@ $(document).ready( function () {
 				$('.theatre-box, .theatre-box .loader').hide();
 				if ( data != undefined ) {
 					$('form input[name=name]').val(data.name).attr('disabled', 'disabled');
+					if ( data.mobile != undefined ) {
+						$('form input[name=mobile]').val(data.mobile);
+					}
 				}
 			});
 		}
@@ -24,27 +27,38 @@ $(document).ready( function () {
 
 	$('form button').on('click', function ( e ) {
 		e.preventDefault();
-		var post = {
-			method: 'confirm',
-			name: $('form input[name=name]').val(),
-			mobile: $('form input[name=mobile').val(),
-			response: ''
-		}
 
-		if ( $('form label a.active').length == 1 ) {
-			if ( $('form label a.active').text().match(/^Yes/) != null ) {
-				post.response = 'yes';
-			} else {
-				post.response = 'no';
+		if ( !$(this).hasClass('err') ) {
+
+			var post = {
+				method: 'confirm',
+				name: $('form input[name=name]').val(),
+				mobile: $('form input[name=mobile').val(),
+				response: ''
 			}
-		}
 
-		if ( post.name != "" && post.response != "" ) {
-			$('.theatre-box, .theatre-box .loader').show();
-			$.post('http://amyporterfield.co/tpw/xhr.php', post, function ( data ) {
-				$('.theatre-box .loader').hide();
-				$('.theatre-box .response, .theatre-box p.' + post.response).show();
-			});
+			if ( $('form label a.active').length == 1 ) {
+				if ( $('form label a.active').text().match(/^Yes/) != null ) {
+					post.response = 'yes';
+				} else {
+					post.response = 'no';
+				}
+			}
+
+			if ( post.name != "" && post.mobile != "" && post.response != "" ) {
+				$('.theatre-box, .theatre-box .loader').show();
+				$.post('http://amyporterfield.co/tpw/xhr.php', post, function ( data ) {
+					$('.theatre-box .loader').hide();
+					$('.theatre-box .response, .theatre-box p.' + post.response).show();
+				});
+			} else {
+				$(this).text('Please complete the form!');
+				$('form button').addClass('err');
+				setTimeout(function () {
+					$('form button').removeClass('err').text('Submit');
+				}, 1000);
+			}
+
 		}
 
 	});
@@ -53,7 +67,30 @@ $(document).ready( function () {
 		e.preventDefault();
 		$('html, body').animate({
 			scrollTop: $('.four').offset().top
-		}, 1000, function () { $('.theatre-box, .theatre-box .response, .theatre-box p, .theatre-box .loader').fadeOut() } )
+		}, 1000, function () { $('.theatre-box .response, .theatre-box p, .theatre-box .loader, .theatre-box .gifts').hide(); $('.theatre-box').fadeOut() } )
 	});
+
+	$("form input[name=mobile]").keydown(function (e) {
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 || (e.keyCode == 65 && e.ctrlKey === true) || (e.keyCode == 67 && e.ctrlKey === true) || (e.keyCode == 88 && e.ctrlKey === true) || (e.keyCode >= 35 && e.keyCode <= 39)) {
+            return;
+        }
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
+
+    $('.four li a').on('click', function ( e ) {
+    	e.preventDefault();
+    	$('.theatre-box .gifts img').attr('src', $(this).parent().find('img').attr('src'));
+    	$('.theatre-box .gifts p').html( '<strong>' + $(this).parent().find('span').text() + '</strong>' + $(this).parent().find('p').text());
+    	$('.theatre-box, .theatre-box .gifts').show();
+    });
+
+    $('.theatre-box .gifts a').on('click', function ( e ) {
+    	e.preventDefault();
+    	$('.theatre-box, .theatre-box .gifts').hide();
+    	$('.theatre-box .gifts img').attr('src', '');
+    	$('.theatre-box .gifts p').text('&nbsp;');
+    });
 
 })
